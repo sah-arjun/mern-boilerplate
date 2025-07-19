a5d670324b54c263daca77735109a81b470d5d05
sah.arjun <sah.arjun07@gmail.com>
2025-07-19 14:22:38 +0545

    cors introduced to allow frontend

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,7 +6,10 @@ import { notFound, errorHandler } from './middlewares/error.middleware';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
 app.use(express.json());
 app.use(logger);
 
